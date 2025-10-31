7527dfdc060c458514f3f5af0ed153c96a81f322
anshpandey2205 <anshupandey2227@gmail.com>
2025-10-31 10:39:13 +0530

    .

--- c_training/Recurrsion/palindrome.c
+++ c_training/Recurrsion/palindrome.c
@@ -1,4 +1,18 @@
-#include<stdio>
+#include<stdio.h>
+int palindrome(int arr[],int i,int j){
+    if(i>=j){
+        return 1;
+    }
+    if(arr[i]!=arr[j]){
+        return 0;
+    }
+    return palindrome(arr,++i,--j);
+}
 int main(){
-    
+ int arr[3]={1,2,1};
+ int startIndex=0;
+ int endIndex=2;
+ int data = palindrome(arr,startIndex,endIndex);
+ printf("%d",data);
+
 }
